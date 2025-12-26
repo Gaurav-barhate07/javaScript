fce2f8b8e352e3cf37a2f4974d5dd88b72e32a42
Gaurav-barhate07 <gauravbarhate127@gmail.com>
2025-12-26 11:18:44 +0530

    add

---
+++ javascript/logicalop.js
@@ -0,0 +1,22 @@
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(10,20);
+// add(20,30);
+// add(30,40);
+// add(40,50);
+
+// function geet(){
+//     console.log("hello gaurav barhate")
+// }
+// geet();
+
+// function geet(name,father,surname){
+//     console.log("Name : "+name);
+//     console.log("father : "+father);
+//     console.log("surname : "+surname);
+//     document.getElementById("enter").innerHTML.geet("");
+// }
+// geet("","","");
+
+console.log(9 > 9)
