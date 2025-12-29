4f8c630ec9f0eb188cac6c05f4a3738f2ef6f2aa
Gaurav-barhate07 <gauravbarhate127@gmail.com>
2025-12-29 15:49:22 +0530

    add new folder

---
+++ javascript/Operators/Arithmetic.js
@@ -0,0 +1,25 @@
+let a = 10;
+let b = 20;
+console.log(a +b)
+
+let c =10;
+let d =20;
+console.log(c-d)
+
+let e=10;
+let f = 20;
+console.log(e*f)
+
+
+let r = 10;
+let h = 20;
+console.log(r /h)
+
+let s = 10;
+let l = 20;
+console.log(s %l)
+
+let u = 10;
+let i = 20;
+console.log(u **i)
+
