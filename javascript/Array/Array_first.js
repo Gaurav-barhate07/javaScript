728e4566acb28c03d14cede08e49ffe2bc337fb2
Gaurav-barhate07 <gauravbarhate127@gmail.com>
2026-01-09 13:34:29 +0530

    new add

---
+++ javascript/Array/Array_first.js
@@ -0,0 +1,18 @@
+let arr =["Gaurav","Aniket","Akshya","Tejus"]
+// console.log(arr);
+// this is print all array
+// and this is array number show your number and print the array
+// console.log(arr[3]);
+// console.log(arr[2]);
+// console.log(arr[1]);
+// console.log(arr[0]);
+
+// arr.length
+// console.log(arr.length)
+
+//arr(push)
+// let add = arr.push("gourav")
+// console.log(add)
+
+
+
