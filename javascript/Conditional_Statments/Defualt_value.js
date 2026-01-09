5a6fd9d9fabc54371e3781e3f83e20cb004d5673
Gaurav-barhate07 <gauravbarhate127@gmail.com>
2026-01-09 13:38:02 +0530

    First_if_else

---
+++ javascript/Conditional_Statments/Defualt_value.js
@@ -0,0 +1,10 @@
+console.log("hello");
+
+
+add();
+function add(v,v2){
+    console.log("hii "+v+v2);
+    
+}
+add("Akshya");
+add("Aniket");
--- javascript/Conditional_Statments/First_if_else.js
+++ javascript/Conditional_Statments/First_if_else.js
@@ -13,3 +13,34 @@
 // let a=1;
 // let y= 20;
 // console.log(Number(a)+b)
+
+// let marks= 35;
+// if(marks>=90 ){
+//     console.log("A Pass");
+// }
+// else if(marks>= 80 ){
+//     console.log("B Pass")
+// }
+// else if(marks >= 70){
+//     console.log("C Pass")
+// }
+// else{
+//     console.log("file")
+// }
+
+
+// let num1 = 10;
+// let num2 = 20;
+// let num3 = 30;
+// let num4 = 40;
+// if (num1 <= num2 ) {
+//     console.log("num1 is big")
+   
+    
+// } else if( num2 >= num3){
+//    console.log("num3 is big")
+    
+// }
+// else if(num3 >= num4){
+//           console.log("num4 is big")
+// }
