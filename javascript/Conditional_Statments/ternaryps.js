697c930d09765a9f82ad76845296319301fbbaa4
Gaurav-barhate07 <gauravbarhate127@gmail.com>
2026-01-09 13:46:06 +0530

    new

---
+++ javascript/Conditional_Statments/ternaryps.js
@@ -0,0 +1,24 @@
+// let marks =90;
+// if(marks > 35){
+//     // result="pass";
+// }
+// else{
+//     // result="file"
+// }
+// // console.log("Result is : " +result);
+
+
+// this is ternary operators
+
+// let number = 25;
+// let result;
+// result = number>=35 ? "pass":"file";
+// console.log(result);
+
+
+
+// this is a nested tenary operators
+let number = 82;
+let result;
+result = number>90 ? "A" : number>80 ? "B":"c";
+console.log(result);
