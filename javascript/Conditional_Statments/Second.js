64b218cf75ed29fd27f302c7fec0aa5855386ef9
Gaurav-barhate07 <gauravbarhate127@gmail.com>
2026-01-09 13:39:01 +0530

    second

--- javascript/Conditional_Statments/Second.js
+++ javascript/Conditional_Statments/Second.js
@@ -2,6 +2,8 @@ var  number1 = 54;
 var number2 = 78;
 var number3 = 80;
 var number4= 120;
+// false || false => false
+// True || True => true 
 if(number1 <number2 || number3<number4){
     console.log("true")
 }
