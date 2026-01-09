74ae66e48da87fd5d803f1b4ca99ec8cc0756444
Gaurav-barhate07 <gauravbarhate127@gmail.com>
2026-01-09 13:43:25 +0530

    FallBack

---
+++ javascript/Conditional_Statments/FallBack.js
@@ -0,0 +1,20 @@
+// console.log("hello");
+
+// falsey value FallBack
+// Nullish Coealscion
+// defulat value (ES2020)
+
+// let username =undefined;
+// let name = username || "gaurav";
+// console.log(name);
+//  differance from ||
+// || teast = "",0,false
+// ?? teast only = null & undefinde
+
+// let marks = undefined;
+// result =(marks>=35)? "pass":"file";
+// console.log(result);
+ let  result = undefined;
+ let cong = result;
+ console.log(cong +100);
+ 
