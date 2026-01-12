c388a5b9c9dcc01dea2e6baa7dc507cbb88bcd99
Gaurav-barhate07 <gauravbarhate127@gmail.com>
2026-01-12 12:03:18 +0530

    unshift

---
+++ javascript/Array/unshift.js
@@ -0,0 +1,47 @@
+let arr= [1,2,3];
+arr.unshift(0);
+console.log(arr);
+// this is adding a starting new value
+
+console.log("********************************");
+
+let aar=[1,2,3,4];
+aar.shift();
+console.log(aar);
+// this is  remove first value
+
+console.log("********************************");
+   
+let arrr= ["apple "," mango"];
+console.log(arrr.join(" , "));
+//  this add  array to string
+
+console.log("********************************");
+
+let nums=[1,2,3,4];
+console.log(nums.toString("gaurav"));
+
+
+console.log("********************************");
+
+let sech= [10,20,30];
+console.log(sech.includes(20));
+// this method is use to values are true or false
+
+console.log("********************************");
+
+let ara=["a","c","d"];
+console.log(ara.indexOf("c"));
+
+console.log("********************************");
+ let fin=[5,12,30];
+ let result = fin.find(n=>n>29);
+ console.log(result);
+//  this is fine to the value show tyou in result 
+ 
+console.log("********************************");
+
+let num=[1,2,3,5,12,25];
+console.log(num.findIndex(n=> n>24));
+
+// this is show the value in under the numbers
